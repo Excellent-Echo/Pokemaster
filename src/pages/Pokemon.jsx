ff14a2620003386fe8a16a01bcb98ee26033488c
mohammedazez <muhamadaziz047@gmail.com>
2021-04-11 23:07:47 +0700

    add  search functionality

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,23 +1,17 @@
-import React, { useEffect } from "react";
-
-// react router
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-
-// react redux
 import { useSelector, useDispatch } from "react-redux";
-
-// action
 import pokemonAction from "../redux/actions/pokemonActions";
-
-import styled from "styled-components";
+// import styled from "styled-components";
 
 const Pokemon = () => {
+  const [search, setSearch] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
 
   // show data pokemon api
   const pokemon = useSelector((state) => state.pokemon.dataApi);
-  console.log("Ini list pokemon pokemon.jsx", pokemon);
+  // console.log("Ini list pokemon pokemon.jsx", pokemon);
   useEffect(() => {
     dispatch(pokemonAction.fetchPokemon());
   }, [dispatch]);
@@ -25,6 +19,12 @@ const Pokemon = () => {
   // klik menuju detail
   const handleClick = (name) => {
     history.push(`/pokemondetail/${name}`);
+  };
+
+  // cari pokemon
+  const handleSearch = (e) => {
+    // console.log(e.target.value);
+    setSearch(e.target.value);
   };
 
   const FavButton = styled.button`
@@ -52,8 +52,8 @@ const Pokemon = () => {
             <div className="col-md-6">
               <h1 className="display-1 title">Pokédex</h1>
               <p className="subtitle">
-                Search for your favourite Pokémon using its name or its National
-                Pokédex number!
+                Search for your favourite Pokémon and fill and collect your
+                favorite pokemon list
               </p>
             </div>
           </div>
@@ -64,14 +64,15 @@ const Pokemon = () => {
           <div className="row">
             <div className="col-md-6">
               <label htmlFor="frm-search" className="form-label src-label">
-                <h3>Search by Name or Number</h3>
+                <h3>Search by Name </h3>
               </label>
               <input
                 className="form-control form-control-lg"
                 id="frm-search"
                 type="text"
-                placeholder="Pikachu or 61"
+                placeholder="Example Pineco"
                 aria-label="Name or Number"
+                onChange={handleSearch}
               />
             </div>
           </div>
@@ -81,29 +82,37 @@ const Pokemon = () => {
         <div className="container">
           <div className="row">
             {pokemon.results &&
-              pokemon.results.map((item, index) => (
-                <div className="col-sm crd-col" key={index}>
-                  <div
-                    className="card poke-card"
-                    style={{ width: "18rem", height: "20rem" }}
-                    onClick={() => handleClick(item.name)}
-                  >
-                    <div className="img-container">
-                      <img
-                        src={`https://img.pokemondb.net/artwork/large/${item.name}.jpg`}
-                        className="card-img-top poke-img"
-                        alt="..."
-                      />
+              pokemon.results
+                .filter((item) => {
+                  return item === ""
+                    ? item
+                    : item.name.toLowerCase().includes(search.toLowerCase());
+                })
+                .map((item, index) => {
+                  return (
+                    <div className="col-sm crd-col" key={index}>
+                      <div
+                        className="card poke-card"
+                        style={{ width: "18rem", height: "20rem" }}
+                        onClick={() => handleClick(item.name)}
+                      >
+                        <div className="img-container">
+                          <img
+                            src={`https://img.pokemondb.net/artwork/large/${item.name}.jpg`}
+                            className="card-img-top poke-img"
+                            alt="..."
+                          />
+                        </div>
+                        <div className="card-body">
+                          <h4 className="card-title">{item.name}</h4>
+                          <span className="badge crd-span" id="electric-type">
+                            Electric
+                          </span>
+                        </div>
+                      </div>
                     </div>
-                    <div className="card-body">
-                      <h4 className="card-title">{item.name}</h4>
-                      <span className="badge crd-span" id="electric-type">
-                        Electric
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                  );
+                })}
           </div>
         </div>
       </div>
