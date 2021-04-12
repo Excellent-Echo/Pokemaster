0289c508ae9d0bc97ed502115baf2c6bd4aa16bf
mohammedazez <muhamadaziz047@gmail.com>
2021-04-12 21:28:07 +0700

    cleaning comment

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -2,47 +2,26 @@ import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import pokemonAction from "../redux/actions/pokemonActions";
-// import styled from "styled-components";
 
 const Pokemon = () => {
   const [search, setSearch] = useState("");
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // show data pokemon api
   const pokemon = useSelector((state) => state.pokemon.dataApi);
   // console.log("Ini list pokemon pokemon.jsx", pokemon);
   useEffect(() => {
     dispatch(pokemonAction.fetchPokemon());
   }, [dispatch]);
 
-  // klik menuju detail
   const handleClick = (name) => {
     history.push(`/pokemondetail/${name}`);
   };
 
-  // cari pokemon
   const handleSearch = (e) => {
     // console.log(e.target.value);
     setSearch(e.target.value);
   };
-
-  // const FavButton = styled.button`
-  //   cursor: pointer;
-  //   background: transparent;
-  //   font-size: 14px;
-  //   border-radius: 5px;
-  //   border: 3px solid #c1292e;
-  //   padding: 0.25em 0.5em;
-  //   transition: 0.2s all ease-out;
-  //   color: #c1292e;
-  //   font-weight: bold;
-
-  //   &:hover {
-  //     background-color: #c1292e;
-  //     color: #f4f4f6;
-  //   }
-  // `;
 
   return (
     <>
@@ -104,7 +83,10 @@ const Pokemon = () => {
                         </div>
                         <div className="card-body">
                           <h4 className="card-title">{item.name}</h4>
-                          <span className="badge card-text crd-span" id="electric-type">
+                          <span
+                            className="badge card-text crd-span"
+                            id="electric-type"
+                          >
                             Electric
                           </span>
                         </div>
